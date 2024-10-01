8e42c90499fd75f4ca4863a52972494ebeb219e5
unknown <irayuliansyah@gmail.com>
2024-10-01 16:51:49 +0700

    autentikasi dan otorisasi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,64 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+@extends('layouts.auth')
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/dist/css/adminlte.min.css') }}">
-</head>
-<body class="hold-transition login-page">
+@section('login')
 <div class="login-box">
+  
   <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="#" class="h1"><b>Admin</b>LTE</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+  <div class="card">
+    <div class="card-body login-card-body">
+    <div class="login-logo">
+    <a href="{{ url('/') }}"><b>
+      <img src="{{ asset('img/logo.jpg') }}" alt="logo.jpg" width="100">
+    </a>
+     </div>
+     
 
-      <!-- Laravel Form -->
-      <form method="POST" action="{{ route('login') }}">
+      <form action="{{ route('login') }}" method="post">
         @csrf
-
-        <!-- Email Address -->
-        <div class="input-group mb-3">
-          <input type="email" id="email" class="form-control" placeholder="Email" name="email" :value="old('email')" required autofocus autocomplete="username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+        <div class="form-group has-feedback @error('email') has-error @enderror">
+                <input type="email" name="email" class="form-control" placeholder="Email" required value="{{ old('email') }}" autofocus>
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                @error('email')
+                    <span class="help-block">{{ $message }}</span>
+                @else
+                <span class="help-block with-errors"></span>
+                @enderror
             </div>
-          </div>
-        </div>
-        @if($errors->has('email'))
-          <span class="text-danger">{{ $errors->first('email') }}</span>
-        @endif
-
-        <!-- Password -->
-        <div class="input-group mb-3">
-          <input type="password" id="password" class="form-control" placeholder="Password" name="password" required autocomplete="current-password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+            <div class="form-group has-feedback @error('password') has-error @enderror">
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                @error('password')
+                    <span class="help-block">{{ $message }}</span>
+                @else
+                    <span class="help-block with-errors"></span>
+                @enderror
             </div>
-          </div>
-        </div>
-        @if($errors->has('password'))
-          <span class="text-danger">{{ $errors->first('password') }}</span>
-        @endif
-
-        <!-- Remember Me -->
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
-              <input type="checkbox" id="remember" name="remember">
+              <input type="checkbox" id="remember">
               <label for="remember">
                 Remember Me
               </label>
@@ -72,35 +50,9 @@
         </div>
       </form>
 
-      <!-- Social Auth Links (optional, can be removed if not needed) -->
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-
-      <!-- Links -->
-      <p class="mb-1">
-        <a href="{{ route('password.request') }}">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-      </p>
-    </div>
-    <!-- /.card-body -->
+      
+    <!-- /.login-card-body -->
   </div>
-  <!-- /.card -->
 </div>
 <!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="{{ asset('AdminLTE-3.2.0/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('/AdminLTE-3.2.0plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('AdminLTE-3.2.0/dist/js/adminlte.min.js') }}"></script>
-</body>
-</html>
+@endsection
